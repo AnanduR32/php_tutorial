46348fc1910e50f3d1464b1ee452aaf312dda944
Anandu R <32507674+AnanduR32@users.noreply.github.com>
2019-02-28 16:34:31 +0530

    Add files via upload

---
+++ save_data.php
@@ -0,0 +1,14 @@
+<?php
+	$conn = new mysqli("localhost","root",'',"ar32");
+	if($conn->connect_error){
+		die("connection failed: ".$conn->connect_eror);
+	}
+	$sql="INSERT INTO register(name,address,gender) VALUES ('$_GET[txtname]','$_GET[txtaddress]','$_GET[gender]')";
+	if($conn->query($sql)===TRUE){
+		echo "New record created succesfully";
+	}
+	else{
+		echo "Error: ".$sql."<br>".$conn->error;
+	}
+	$conn->close();
+?>
