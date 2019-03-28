c50ea8466c0e7a545c4c8291449606be91b644f8
Anandu R <32507674+AnanduR32@users.noreply.github.com>
2019-03-28 16:30:50 +0530

    Add files via upload

--- save_data.php
+++ save_data.php
@@ -1,14 +1,21 @@
 <?php
+if(isset($_POST['submit'])){
 	$conn = new mysqli("localhost","root",'',"ar32");
 	if($conn->connect_error){
 		die("connection failed: ".$conn->connect_eror);
 	}
-	$sql="INSERT INTO registration(Name,Address,Gender)VALUES('$_GET[txtname]','$_GET[txtaddress]','$_GET[gender]')";
-	if($conn->query($sql)===TRUE){
-		echo "New record created succesfully";
+	$sql_check="SELECT * from register where name like '_POST[txtname]' and Password like '_POST[Password]'";
+	$result=$conn->query($sql_check);
+	if($result->num_rows>0){
+		echo "This username and password already Exists";
 	}
-	else{
-		echo "Error: ".$sql."<br>".$conn->error;
-	}
-	$conn->close();
+	else if($result->num_rows==0){
+		$sql="INSERT INTO registration(Name,Address,Gender,Password,Role)VALUES('$_GET[txtname]','$_GET[txtaddress]','$_GET[gender]','$_GET[Password]','$_GET[Role]')";
+		if($conn->query($sql)===TRUE){
+			echo "New record created succesfully";
+		}
+		else{
+			echo "Error: ".$sql."<br>".$conn->error;
+		}
+		$conn->close();
 ?>
