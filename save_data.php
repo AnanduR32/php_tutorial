63ac7feaa1e93551723d8a262f50f339b4e27699
Anandu R <32507674+AnanduR32@users.noreply.github.com>
2019-04-04 12:07:09 +0530

    Add files via upload

--- save_data.php
+++ save_data.php
@@ -1,21 +1,23 @@
 <?php
-if(isset($_POST['submit'])){
-	$conn = new mysqli("localhost","root",'',"ar32");
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$database="ar32";
+	$conn = new mysqli($servername,$username,$password,$database);
 	if($conn->connect_error){
 		die("connection failed: ".$conn->connect_eror);
 	}
-	$sql_check="SELECT * from register where name like '_POST[txtname]' and Password like '_POST[Password]'";
-	$result=$conn->query($sql_check);
-	if($result->num_rows>0){
-		echo "This username and password already Exists";
+	$name=$_GET['txtname'];
+	$addr=$_GET['txtaddress'];
+	$mail=$_GET['txtmail'];
+	$dob=$_GET['txtdob'];
+	$sex=$_GET['gender'];
+	$sql="INSERT INTO registration(name,addr,email,dob,gender)VALUES('$name','$addr','$mail','$dob','$sex')";
+	if($conn->query($sql)===TRUE){
+		echo "New record created succesfully";
 	}
-	else if($result->num_rows==0){
-		$sql="INSERT INTO registration(Name,Address,Gender,Password,Role)VALUES('$_GET[txtname]','$_GET[txtaddress]','$_GET[gender]','$_GET[Password]','$_GET[Role]')";
-		if($conn->query($sql)===TRUE){
-			echo "New record created succesfully";
-		}
-		else{
-			echo "Error: ".$sql."<br>".$conn->error;
-		}
-		$conn->close();
+	else{
+		echo "Error: ".$sql."<br>".$conn->error;
+	}
+	$conn->close();
 ?>
